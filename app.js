ea7513e0e0e20fd1af6c83c0e1e4e6d629e35797
vaisajuh <juha.vaisanen@gmail.com>
2023-02-18 18:25:05 +0200

    42

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/blogs', blogsRouter)
+
+module.exports = app
